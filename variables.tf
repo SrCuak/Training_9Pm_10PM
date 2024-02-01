1990f4c7c253026ccecf2181df4e6c6bae9644fe
SrCuak <156462037+SrCuak@users.noreply.github.com>
2024-02-01 21:41:27 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "AWS_REGION" {
+    default = "ap-south-1" 
+}
+
+variable "AWS_ACCESS_KEY" {
+  type = string
+  default = "AKIAYDDHIM3ZH5NBDS4A"
+}
+variable "AWS_SECRET_KEY" {
+    type = string
+    default = "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"
+  
+}
